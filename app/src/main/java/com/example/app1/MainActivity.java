95791d8c5c1122b8e1158e88a2cc44c9a50b47b1
kaconk <harutonouciha@gmail.com>
2020-03-06 23:13:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ToPage2(View view) {
+        Intent next = new Intent(this, Page2.class);
+        startActivity(next);
+    }
+}
