b31bd503034b7680f3029b6d2fc6f51b88f045fc
kaconk <harutonouciha@gmail.com>
2020-03-07 07:45:37 +0700

    halaman about

--- app/src/main/java/com/example/app1/Page2.java
+++ app/src/main/java/com/example/app1/Page2.java
@@ -10,5 +10,6 @@ public class Page2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+        setTitle("About");
     }
 }
