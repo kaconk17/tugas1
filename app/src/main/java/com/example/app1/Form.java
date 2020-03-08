af33ed20c09ee29da33e68981989a3858c5146ef
kaconk <harutonouciha@gmail.com>
2020-03-08 22:31:50 +0700

    setting datetimepicker

--- app/src/main/java/com/example/app1/Form.java
+++ app/src/main/java/com/example/app1/Form.java
@@ -1,14 +1,53 @@
 package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+//import android.widget.EditText;
+
+
+import java.util.Calendar;
 
 public class Form extends AppCompatActivity {
+    //EditText tgl_lahir = findViewById(R.id.textTgl);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        setTitle("Formulir");
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // mengambil waktu saat ini sebagai nilai awal
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+           
+
+        }
     }
 }
