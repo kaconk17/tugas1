d40cf76e44e052c3ddbe5b03e6646a3b3cd29232
kaconk <harutonouciha@gmail.com>
2020-03-10 22:47:53 +0700

    datepicker fix

--- app/src/main/java/com/example/app1/Form.java
+++ app/src/main/java/com/example/app1/Form.java
@@ -1,53 +1,48 @@
 package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
-//import android.widget.EditText;
+import android.widget.EditText;
 
 
 import java.util.Calendar;
 
 public class Form extends AppCompatActivity {
-    //EditText tgl_lahir = findViewById(R.id.textTgl);
+
+    EditText tgl_lahir;
+    DatePickerDialog picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
         setTitle("Formulir");
-    }
 
-    public void showDatePicker(View view) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
-    }
+        tgl_lahir = (EditText) findViewById(R.id.textTgl);
+        tgl_lahir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                final Calendar cldr = Calendar.getInstance();
+                final int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
 
-    public static class DatePickerFragment extends DialogFragment
-            implements DatePickerDialog.OnDateSetListener {
+                picker = new DatePickerDialog(Form.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfyear, int dayOfMonth) {
+                        tgl_lahir.setText(year+"-"+monthOfyear+"-"+dayOfMonth);
+                    }
+                }, year, month,day);
+                picker.show();
+            }
 
+        });
 
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // mengambil waktu saat ini sebagai nilai awal
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
-        }
-
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-           
-
-        }
     }
 }
